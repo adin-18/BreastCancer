d5310450695945395ef34449bb7964d76d8b046e
Adin Firoz <firozzoya6@gmail.com>
2024-09-05 00:12:58 +0530

    Intro activity

---
+++ app/src/main/java/com/example/breastcancer/SplashScreen.java
@@ -0,0 +1,44 @@
+package com.example.breastcancer;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+
+        runNextScreen();
+        askForFullScreen();
+    }
+
+    private void askForFullScreen() {
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                View.SYSTEM_UI_FLAG_FULLSCREEN |
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+        );
+    }
+
+    private void runNextScreen() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, IntroActivity.class));
+                finish();
+            }
+        }, 2500);
+    }
+
+}
