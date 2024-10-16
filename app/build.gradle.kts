2aad9d49ebe3fad0bf6dd255781cff3994271d35
Adin Firoz <firozzoya6@gmail.com>
2024-10-17 01:01:08 +0530

    TF Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,4 +62,6 @@ dependencies {
     implementation ("com.github.bumptech.glide:glide:4.15.0") // Check for the latest version
     annotationProcessor ("com.github.bumptech.glide:compiler:4.15.0") // For annotation processing
     implementation ("de.hdodenhof:circleimageview:3.1.0") // Check for the latest version
+    implementation ("org.tensorflow:tensorflow-lite:2.12.0") // or latest version
+
 }
